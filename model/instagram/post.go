02fb2c70a82be3430341e65cf47ceda69c95cd75
leebumseok <qpwoeiru6486@gmail.com>
2020-11-20 23:58:48 +0900

    Remove unnecessary comments

--- model/instagram/post.go
+++ model/instagram/post.go
@@ -1,19 +1,5 @@
 package instagram
 
-// type Post struct {
-// 	Graphql struct {
-// 		ShortcodeMedia struct {
-// 			EdgeMediaPreviewLike struct {
-// 				Count int `json:"count"`
-// 			} `json:"edge_media_preview_like"`
-// 			Location struct {
-// 				Name        string `json:"name"`
-// 				AddressJSON string `json:"address_json"`
-// 			} `json:"location"`
-// 		} `json:"shortcode_media"`
-// 	} `json:"graphql"`
-// }
-
 type Post struct {
 	Graphql struct {
 		ShortcodeMedia struct {
